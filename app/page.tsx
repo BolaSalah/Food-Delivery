bf8bada3eebb5a982f1df6fd259415b8371808d2
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-09-20 03:03:04 +0300

    add menu section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
+import { useState } from "react";
 
 export default function Home() {
+  const [ category, setCategory ] = useState("All");
   return (
     <>
       <Hero />
+      <Menu category={category} setCategory = {setCategory}/>
     </>
   );
 }
---
+++ components/Menu.tsx
@@ -0,0 +1,42 @@
+import { menu_list } from './../assets/assets';
+import Image from 'next/image';
+import React, { Dispatch, SetStateAction } from 'react';
+
+type propsType = {
+  category: string;
+  setCategory: Dispatch<SetStateAction<string>>;
+};
+
+const Menu = (props: propsType) => {
+  return (
+    <div className='menu mt-10'>
+      <div className='text-4xl font-medium mb-4'>Explore our menu</div>
+      <div className='md:w-2/3'>
+        Choose from a diverse menu featuring a delectable array of dishes. our
+        mission is to satisfy your cravings and elevate your during experience,
+        one delicious meal at a time.
+      </div>
+      <div className='flex justify-between items-center gap-7 mt-8 overflow-x-scroll lg:overflow-hidden'>
+        {menu_list.map((e, index) => (
+          <div
+            onClick={() => props.setCategory(e.menu_name)}
+            key={index}
+            className='flex flex-col items-center '
+          >
+            <Image
+              src={e.menu_image}
+              alt={e.menu_name}
+              className={`mb-2 cursor-pointer min-w-20 rounded-full ${
+                props.category === e.menu_name ? 'border-4 border-black' : ''
+              }`}
+            />
+            <p>{e.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <div className='h-0.5 bg-[#e2e2e2] my-10'></div>
+    </div>
+  );
+};
+
+export default Menu;
